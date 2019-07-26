ab92dcb9b6acb46ea17c11e5bf50d4df26e5d7a0
david <cfcdavidchan@gmail.com>
2019-07-26 11:00:40 +0800

    create api file for coin market cap

--- csv_download.py
+++ csv_download.py
@@ -19,3 +19,15 @@ else:
 
 today_csv_dir = os.path.join(csv_path, today)
 
+# check api file for coinmarketcap
+api_path = os.path.join(root, 'coinmarketcap_api.txt')
+if not os.path.exists(api_path): #api file not found
+    api = input('Please provide your coin market cap api:\n')
+
+    with open(api_path, "w") as text_file: # api text file create
+        text_file.write(api)
+
+else:
+    with open(api_path, "r") as text_file:
+        api = text_file.read()
+
